f64c71491532a01ed29c897bee4f55212cf85948
Sumed Tamang <xumed001@gmail.com>
2023-06-18 21:31:41 -0500

    6/18/23

---
+++ JS/sequencing_animations/main.js
@@ -0,0 +1,35 @@
+const aliceTumbling = [
+    { transform: 'rotate(0) scale(1)' },
+    { transform: 'rotate(360deg) scale(0)' }
+  ];
+  
+  const aliceTiming = {
+    duration: 2000,
+    iterations: 1,
+    fill: 'forwards'
+  }
+  
+  const alice1 = document.querySelector("#alice1");
+  const alice2 = document.querySelector("#alice2");
+  const alice3 = document.querySelector("#alice3");
+
+
+// Promise chaining 
+// alice1.animate(aliceTumbling, aliceTiming).finished
+//   .then(() => alice2.animate(aliceTumbling, aliceTiming).finished)
+//   .then(() => alice3.animate(aliceTumbling, aliceTiming).finished)
+//   .catch(error => console.error(`Error animating Alices ${error}`))
+
+// async/await
+async function animateAlices(){
+    try{
+        await alice1.animate(aliceTumbling, aliceTiming).finished
+        await alice2.animate(aliceTumbling, aliceTiming).finished
+        await alice3.animate(aliceTumbling, aliceTiming).finished
+    } 
+    catch(error) {
+        console.error(`Error animating Alices ${error}`)
+    }
+}
+
+animateAlices()
